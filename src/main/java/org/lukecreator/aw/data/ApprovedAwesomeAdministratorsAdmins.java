35f49e9b98b5bb439a0a22e1304ce109571709b6
Luke Criswell <luke@criswell.net>
2026-03-08 22:26:00 -0400

    checken ducky luke yucky

--- src/main/java/org/lukecreator/aw/data/ApprovedAwesomeAdministratorsAdmins.java
+++ src/main/java/org/lukecreator/aw/data/ApprovedAwesomeAdministratorsAdmins.java
@@ -2,9 +2,9 @@ package org.lukecreator.aw.data;
 
 public class ApprovedAwesomeAdministratorsAdmins {
     public static final long[] ADMINS = {
-            214183045278728202L, // luke
             348136128932610058L, // checken
             367618656613564418L, // ducky
+            214183045278728202L, // luke
             239526347972673537L  // yucky
     };
 
